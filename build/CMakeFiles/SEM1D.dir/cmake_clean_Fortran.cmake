5052f40d07f00a02248f98db65233635536c1e2a
musbenziane <medbenziane1992@gmail.com>
2021-12-01 16:36:41 +0100

    rm useless files

--- build/CMakeFiles/SEM1D.dir/cmake_clean_Fortran.cmake
+++
@@ -1,6 +0,0 @@
-# Remove fortran modules provided by this target.
-FILE(REMOVE
-  "gll_library.mod"
-  "GLL_LIBRARY.mod"
-  "CMakeFiles/SEM1D.dir/gll_library.mod.stamp"
-  )
--- build/CMakeFiles/SEM1D.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for SEM1D.
-# This may be replaced when dependencies are built.
