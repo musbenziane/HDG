201943df1b96ebcc6c2d228bec2e9dd1029c1952
musbenziane <medbenziane1992@gmail.com>
2021-12-01 15:55:17 +0100

    var DEALLOCATE, submit for check by professor

--- main.f90
+++ main.f90
@@ -1,3 +1,47 @@
+    !###################################################################################################################
+    ! HDG (SEM & DG) Hybrod Solver with a regular mesh for the 1D elastic wave equation 
+    ! NOTE: This program is yet to be tested against analytical solutions.
+    ! 
+    ! Language: Fortran 90, with parralel impelementation using OpenMP API
+    ! 
+    ! Sources:  Igel 2017, Leveque 2002, Hesthaven & Warburton 2008
+    ! 
+    ! The code used for arbitrary GLL points and weights was created by M.I.T departement of engineering. Link is hereafter
+    ! https://geodynamics.org/cig/doxygen/release/specfem3d/    | file name: gll_library.f90
+    ! 
+    ! This is part of the Numerical Modelling Workshop.
+    ! 
+    ! Supervisor: Pr. Emmanual Chaljub
+    ! Author    : Mus Benziane
+    !
+    ! Input file: [Example] [Last BC options are yet to be impelented...]
+    ! testing              ! Model name prefix (model names: prefix_vp, prefix_rho)
+    ! 6                    ! Polynomial order
+    ! 200                  ! Number of elements SEM
+    ! 200                  ! Number of elements DG
+    ! 30.                  ! Element size
+    ! 20.                  ! Wavelet's peak frequency
+    ! 0.0002               ! Time step
+    ! 20000                ! Number of time steps
+    ! 150                  ! Source location (element number)
+    ! 4                    ! Source location (gll point)
+    ! 50                   ! Snapshot interval
+    ! 1                    ! [1/2/3/4] 1: Free surface, 2: Rigid wall, 3: Periodic, 4: Sponge 
+    ! 0                    ! Boundary condition only on the left side [not for periodic BC | on the right side for absorbing]
+    ! 20                   ! Sponge layer width in gll points
+    ! .65                  ! Att constant for sponge layer
+    ! 0                    ! Use intial conditions instead of point source, and produce analytical solution
+    ! 100                  ! Width of Gaussian used as intial condition
+
+
+    ! -> Model files in C-Style binary floats [doubles]: Vs, Rho files are needed.
+    !                                                  : For simple models, use create1Dmodel_files.f90
+    ! 
+    ! -> Outputs are created in OUTPUT/ if OUTPUT/ is not created by the user, the program will not handle it.
+    !    Output files in OUTPUT directory:
+    !
+    !####################################################################################################################
+
 PROGRAM HDG
     
     !$ USE OMP_LIB
@@ -18,11 +62,11 @@ PROGRAM HDG
     TYPE(SETup)                                                :: SET
     TYPE(semProblem)                                           :: SEM
     TYPE(dgProblem)                                            :: DG
-    REAL (KIND=8)                                              :: sum, CFL, mINdist, lambdamIN, taper
-    REAL (KIND=8)                                              :: time, t_cpu_0, t_cpu_1, t_cpu, tmp, tmpBC, attConst, sd
+    REAL (KIND=8)                                              :: CFL, mINdist, lambdamIN
+    REAL (KIND=8)                                              :: time, t_cpu_0, t_cpu_1, t_cpu, attConst, sd
                                                             
     REAL (KIND=8), dimension(:), allocatable                   ::  g
-    INTEGER                                                    :: i, j, k, l, it, el, c, reclsnaps
+    INTEGER                                                    :: i, j, k, it, c, reclsnaps
     INTEGER                                                    :: ir, t0, t1, bc, sbc, gWidth, IC
     character(len=40)                                          :: filename, filecheck, outname, modnameprefix
     !$ INTEGER                                                 :: n_workers
@@ -86,6 +130,9 @@ PROGRAM HDG
     READ(2,*) SET%esrc
     READ(2,*) SET%gsrc
     READ(2,*) SET%isnap
+
+
+    !##### Implementation on going, well copy paste, they have been already implemented on the seperate DG & SEM programs.
     READ(2,*) bc
     READ(2,*) sbc
     READ(2,*) gWidth
@@ -94,14 +141,19 @@ PROGRAM HDG
     READ(2,*) sd
     CLOSE(2)
 
-    PRINT*,"Polynomial order          -> ",SET%N
-    PRINT*,"Number of elements        -> ",SEM%ne
-    PRINT*,"Element size              -> ",SET%h
-    PRINT*,"Wavelet's peak frequency  -> ",SET%f0
-    PRINT*,"Time step                 -> ",SET%dt
-    PRINT*,"Number of time steps      -> ",SET%nt
-    PRINT*,"Source location [nel/ngll]-> ",SET%esrc, SET%gsrc
-    PRINT*,"Snapshot INterval         -> ",SET%isnap
+    120 format (A,F6.1)
+    140 format (A,I4,X,I4)
+    160 format (A,I8)
+
+    write(*,160)  "Polynomial order                   -> ",SET%N
+    write(*,160)  "Number of elements SEM             -> ",SEM%ne
+    write(*,160)  "Number of elements DG              -> ",DG%ne
+    write(*,120)  "Element size                       -> ",SET%h
+    write(*,160)  "Number of time steps               -> ",SET%nt
+    write(*,120)  "Time step                          -> ",SET%dt
+    write(*,120)  "Ricker's peak frequency            -> ",SET%f0
+    write(*,140)  "Source location [nel/ngll]         -> ",SET%esrc, SET%gsrc
+    write(*,160)  "Snapshot interval                  -> ",SET%isnap
 
 
     SEM%ngll  = SET%N * SEM%ne + 1                                  ! Total GLL poINts
@@ -174,6 +226,61 @@ PROGRAM HDG
     ! DG & SEM Matrices setup
     CALL semDefine(SET,SEM)
     CALL dgDefine(SET, DG)
+
+
+
+    write(*,*)"##########################################"
+    write(*,*)"############### CFL Check ################"
+    write(*,*)"##########################################"
+
+    mindist = SEM%xgll(2) - SEM%xgll(1)
+    CFL = (SET%dt/mindist) * maxval(SET%v1D(:))
+
+    if (CFL > .8) then
+        print"(a14,f6.3)","CFL value is ",CFL
+        print*,"Decrease time step, the program has been terminated"
+        stop
+
+    else
+        print"(a14,f6.3)","CFL value is ",CFL
+        print*,"Simulation is stable"
+    end if
+
+
+    write(*,*)"##########################################"
+    write(*,*)"########## Space Sampling check ##########"
+    write(*,*)"##########################################"
+    lambdamin = minval(SET%v1D)/(SET%f0*2.5)
+
+    print"(a32,f3.1)", " Elements per minimum wavelength ->", lambdamin/SET%h
+
+    if ((lambdamin/SET%h)<1) then
+        print*,"Element size is too large"
+        print*,"Numerical dispersion might be present"
+        print*,"Do you wish to continue anyways? Yes/no"
+        read*, filecheck
+
+        if (filecheck=="Yes" .or. filecheck=="yes" .or. filecheck=="y" .or. &
+                filecheck=="Y") then
+            print*,"Proceeding..."
+
+        elseif  (filecheck=="No" .or. filecheck=="no" .or. filecheck=="n" .or. &
+                filecheck=="N") then
+            write(*,*) "The program had been terminated"
+            write(*,*) "Reduce element size or frequency as such"
+            write(*,*) "you have at least 1 element per min wavelength"
+            write(*,*) "This holds for N=5 to N=10 as per"
+            write(*,*) "Komatitsch and Tromp 1999"
+            stop
+        else
+            write(*,*) "Only: Yes/yes/Y/y & No/no/N/n are handled"
+            write(*,*) "The program have been terminated, please star over"
+            stop
+        end if
+    else
+        print*, "Spatial sampling as OK!"
+    end if
+
 
     DO it=1,SET%nt 
         
@@ -269,4 +376,43 @@ PROGRAM HDG
     write(*,*) "##########################################"
 
 
+
+
+
+    DEALLOCATE(SET%xi)                                       
+    DEALLOCATE(SET%wi)                                       
+    DEALLOCATE(SET%Cij)                              
+    DEALLOCATE(SET%v1D)                                
+    DEALLOCATE(SET%rho1D)                                   
+    DEALLOCATE(SET%lprime)                         
+    DEALLOCATE(SEM%rho1Dgll)                                
+    DEALLOCATE(SEM%v1Dgll)                             
+    DEALLOCATE(SEM%M)                                      
+    DEALLOCATE(SEM%MINv)                              
+    DEALLOCATE(SEM%Me)                                     
+    DEALLOCATE(SEM%Kg)                            
+    DEALLOCATE(SEM%Ke)                                 
+    DEALLOCATE(SEM%u)                              
+    DEALLOCATE(SEM%unew, SEM%uddotnew, SEM%udotnew)
+    DEALLOCATE(SEM%uddot, SEM%udot)
+    DEALLOCATE(SEM%sigma, SEM%sigmaold, SEM%sigmanew)
+    DEALLOCATE(SEM%tauL,SEM%tauLold,SEM%tauLnew,SEM%T)
+    DEALLOCATE(SEM%uold)                                           
+    DEALLOCATE(SET%src)                                               
+    DEALLOCATE(SEM%F)                                               
+    DEALLOCATE(SEM%Uout)               
+    DEALLOCATE(SEM%Udotout, SEM%sigmaout)
+    DEALLOCATE(SEM%mu1Dgll)                                         
+    DEALLOCATE(SEM%xgll)                                            
+    DEALLOCATE(g)
+    DEALLOCATE(SEM%recsigma)
+    DEALLOCATE(DG%Z)                                                       
+    DEALLOCATE(DG%xgll)                                                  
+    DEALLOCATE(DG%Minv)                                             
+    DEALLOCATE(DG%Ke)                                                   
+    DEALLOCATE(DG%Ar,DG%Al)                                          
+    DEALLOCATE(DG%u,DG%unew)                              
+    DEALLOCATE(DG%k1,DG%k2,DG%source)
+    DEALLOCATE(DG%flux)                                   
+    DEALLOCATE(DG%sigma, DG%v)
 END PROGRAM
